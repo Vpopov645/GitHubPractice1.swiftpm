38e9d4341b1547c72c606b5f077847f5bae75f14
Vasil <vpopov6335@stu.d214.org>
2023-04-04 14:31:57 -0500

    Added 2 variables for both textfields. Made two textfields that receive these variables.

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1: Int = 0
+    @State var number2: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        TextField("Number 1", value: $number1, formatter: .init())
+        TextField("Number 2", value: $number2, formatter: .init())
     }
 }
