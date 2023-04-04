555a95c13f2d98726901d285c95eebe0edb0913a
Vasil <vpopov6335@stu.d214.org>
2023-04-04 14:52:15 -0500

    Changed Textfield Format

--- ContentView.swift
+++ ContentView.swift
@@ -6,32 +6,28 @@ struct ContentView: View {
     @State var answer: Int = 0 
     
     var body: some View {
-        TextField("Number 1", value: $number1, formatter: .init())
-        TextField("Number 2", value: $number2, formatter: .init())
+        TextField("Number 1", value: $number1, format: .number)
+        TextField("Number 2", value: $number2, format: .number)
         Button("Multiply") {
             answer = number1 * number2
         }
+        .foregroundColor(.blue)
+        .background(.red)
         Button("Subtract") {
             answer = number1 - number2
         }
+        .foregroundColor(.blue)
+        .background(.red)
         Button("Divide") {
             answer = number1 / number2
         }
+        .foregroundColor(.blue)
+        .background(.red)
         Button("Add") {
             answer = number1 + number2 
         }
-       Button("Multiply", action: <#T##() -> Void#>)
-            .foregroundColor(.blue)
-            .background(.red)
-        Button("Subtract", action: <#T##() -> Void#>)
-            .foregroundColor(.blue)
-            .background(.red)
-        Button("Divide", action: <#T##() -> Void#>)
-            .foregroundColor(.blue)
-            .background(.red)
-        Button("Add", action: <#T##() -> Void#>)
-            .foregroundColor(.blue)
-            .background(.red)
+        .foregroundColor(.blue)
+        .background(.red)
         Text("\(answer)")
             .foregroundColor(.orange)
             .background(.green)
