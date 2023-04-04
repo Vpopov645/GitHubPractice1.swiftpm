31b2a1fe633ef8e1ad5f5ff9757bc5f17f33998f
Vasil <vpopov6335@stu.d214.org>
2023-04-04 14:41:52 -0500

    Added Code to multiply  button.

--- ContentView.swift
+++ ContentView.swift
@@ -3,12 +3,14 @@ import SwiftUI
 struct ContentView: View {
     @State var number1: Int = 0
     @State var number2: Int = 0
-    @State var answer: Int
+    @State var answer: Int = 0 
     
     var body: some View {
         TextField("Number 1", value: $number1, formatter: .init())
         TextField("Number 2", value: $number2, formatter: .init())
-       Button("Multiply", action: <#T##() -> Void#>)
+        Button("Multiply") {
+            answer = number1 * number2
+        }
         Text("\(answer)")
     }
 }
