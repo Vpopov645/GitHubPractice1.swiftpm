32a5811b1771560a69a50c5456f1838c7f34d078
Jake <jvoris6444@stu.d214.org>
2023-04-04 14:39:18 -0500

    I added 3 more buttons. Add, subtract, and divide.

--- ContentView.swift
+++ ContentView.swift
@@ -9,6 +9,9 @@ struct ContentView: View {
         TextField("Number 1", value: $number1, formatter: .init())
         TextField("Number 2", value: $number2, formatter: .init())
        Button("Multiply", action: <#T##() -> Void#>)
+        Button("Subtract", action: <#T##() -> Void#>)
+        Button("Divide", action: <#T##() -> Void#>)
+        Button("Add", action: <#T##() -> Void#>)
         Text("\(answer)")
     }
 }
