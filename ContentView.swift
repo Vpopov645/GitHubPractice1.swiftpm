22963753c2405943a5ad79756aba108ae07c5776
Jake <jvoris6444@stu.d214.org>
2023-04-04 14:31:44 -0500

    I added a button and a text that displays the answer.

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answer: Int
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+       Button("Multiply", action: <#T##() -> Void#>)
+        Text("\(answer)")
     }
 }
